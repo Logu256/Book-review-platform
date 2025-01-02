0ae7a863e6121920eddcf407ce7009d61be830ee
Logu256 <logananth.kamalasekarannpt@gmail.com>
2025-01-02 14:17:40 +0530

    Create Review.java

---
+++ Review.java
@@ -0,0 +1,18 @@
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private String review;
+    private int rating;
+    private Timestamp createdAt;
+}
